4003d61e4697837b665072fae814f32cde6dd7e1
Noa <molinasarthez@gmail.com>
2025-10-23 14:29:52 +0200

    Numbers 0 bis 999 erledigt

--- natural/number_string_6_digits.go
+++ natural/number_string_6_digits.go
@@ -2,6 +2,7 @@ package natural
 
 // NumberString6Digits erwartet eine Zahl 0 <= n <= 999999 und liefert den zugehörigen String.
 func NumberString6Digits(n int) string {
-	// TODO
+	//TODO
+
 	return ""
 }
